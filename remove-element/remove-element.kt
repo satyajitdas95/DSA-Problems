6632b72afdf16c966d367b2f7198f5b8b35fdeeb
Satyajit Das <dassatya1995@gmail.com>
2023-05-15 13:23:20 +0530

    Time: 155 ms (85.39%) | Memory: 36.2 MB (17.74%) - LeetSync

--- remove-element/remove-element.kt
+++ remove-element/remove-element.kt
@@ -1,24 +1,12 @@
 class Solution {
     fun removeElement(nums: IntArray, target: Int): Int {
-        val indexMap = mutableListOf<Int>()
-        var k=nums.size
-
-        nums.forEachIndexed{index,num->
-
-        if(num == target){
-            indexMap.add(index) //3 
-            k-- //7 6 
-        } else if(num != target){
-            if(indexMap.size>0){ 
-                nums[indexMap[0]] = num
-                indexMap.removeAt(0)
-                indexMap.add(index)
-            }
-        }
-
-        }
-
-        return k
-        
+       var k =0
+       nums.forEach{num->
+       if(num != target) {
+           nums[k++] = num
+       }
     }
+
+    return k
+  }
 }
